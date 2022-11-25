3db409eeb3849e8fe28c16b45f352b68bfab6521
Rodrigo Montalvao <montalvao@gmail.com>
2022-11-25 08:45:18 -0300

    Flask Skeleton v.0.2.0

--- app/setup.py
+++ app/setup.py
@@ -7,7 +7,7 @@ def read(filename: str) -> list[str]:
 
 setup(
     name="skel",
-    version="0.1.0",
+    version="0.2.0",
     description="skel-description",
     packages=find_packages(where="app"),
     include_package_data=True,
