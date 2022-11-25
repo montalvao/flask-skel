ae0d1dc3db2f923be9eecdc03a628b650fd3a0f5
Rodrigo Montalvao <montalvao@gmail.com>
2022-11-25 06:15:42 -0300

    Change pytest fixture scope to speed up tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,12 +3,12 @@ import pytest
 from skel.app import create_app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def app():
     app = create_app(force_env="testing")
     return app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def simple_routes(app):
     return [str(r) for r in app.url_map.iter_rules() if "<" not in str(r)]
