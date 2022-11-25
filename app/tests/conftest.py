dd7376b512b746d064c81f6f77cdd80e13940db0
Rodrigo Montalvao <montalvao@gmail.com>
2022-11-25 06:16:29 -0300

    Add test header

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,6 +3,11 @@ import pytest
 from skel.app import create_app
 
 
+def pytest_report_header(config):
+    header_text = f"Project skell.app."
+    return header_text
+
+
 @pytest.fixture(scope="session")
 def app():
     app = create_app(force_env="testing")
